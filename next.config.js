dd51a4c5953a19010aee9d644023f79288672f5c
Hardik <hardikdharmik94@gmail.com>
2022-12-29 20:43:34 +0530

    Header added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["upload.wikimedia.org", "links.papareacr.com", "www.pngfind.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
